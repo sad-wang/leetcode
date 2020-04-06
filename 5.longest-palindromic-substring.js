bab80126f2e79f72596384337a0a5918ded0bc09
sad-wang <leown_wang@163.com>
2020-04-07 00:55:46 +0800

    update

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -38,7 +38,38 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-
+{
+    // 通过遍历字符串，寻找 index 或 index，index-1 为中心的回文串
+    let longestPalindrome = function(s) {
+        if (!s) return ''
+        if (s.length == 1) return s
+        let maxLength = 0
+        let result = ''
+        let callback = (str, length) => {
+            result = str
+            maxLength = length
+        }
+        for (let i = 1; i < s.length; i++){
+            let oddStr = checkPalindromic(s, i, i)
+            let evenStr = checkPalindromic(s, i-1, i)
+            replaceMaxLengthAndResult(oddStr, maxLength, callback)
+            replaceMaxLengthAndResult(evenStr, maxLength, callback)
+        }
+        return result
+    }
+    function replaceMaxLengthAndResult(str, maxLength,callback) {
+        if (str.length > maxLength){
+            callback(str, str.length)
+        }
+    }
+    function checkPalindromic(s, left, right) {
+        let str = ''
+        while (s.charAt(left) == s.charAt(right) && left >= 0 && right < s.length){
+            str = s.substring(left, right+1)
+            left--
+            right++
+        }
+        return str
+    }
 }
 // @lc code=end
